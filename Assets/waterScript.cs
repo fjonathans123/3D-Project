4cbdf15feaf48731d1ded7015f39a7491ede103e
Jonathan <frederickjsoenarwan@gmail.com>
2026-02-04 22:50:00 +0800

    water + lights

---
+++ Assets/waterScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterScript : MonoBehaviour
+{
+    public bool isWater = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+        {
+        if (other.CompareTag("Water"))
+        {
+            isWater = true;
+        }
+    }
+
+    // Update is called once per frame
+     private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            isWater = false;
+        }
+    }
+}
